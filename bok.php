236aeb362993953b1c125fa2494e14c5eb78cf22
sveinnmani <sveinnmani@hotmail.com>
2016-10-13 17:02:53 +0000

    Add files via upload

---
+++ bok.php
@@ -0,0 +1,21 @@
+<?php
+	class Bok {
+		public $titill = '';
+		public $verd;
+		$efnafraedi = (object) array('titill' => titill, 'verd' => verd);
+		$staerdfraedi = (object) array('titill' => titill, 'verd' => verd);
+		$islenska = (object) array('titill' => titill, 'verd' => verd);
+		function setPrice()	{
+			
+		}
+		function getPrice()	{
+			
+		}
+		function setTitle()	{
+			
+		}
+		function getTitle()	{
+			
+		}
+	}
+?>
